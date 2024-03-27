4ac7f01c9a79f96d91e77f68126e29a88958277b
Kenan Kartal <contact@kenankartal.name>
2024-03-27 20:47:42 +0300

    Add 01_04-exit

---
+++ 01_04-exit/exit.s
@@ -0,0 +1,15 @@
+# Program: exit
+#
+# Executes the exit system call.
+#
+# No input.
+#
+# Output: only the exit status ($? in the shell)
+#
+.section .text
+.global _start
+_start:
+        mov $1, %eax    # 1 is the exit syscall number
+        mov $5, %ebx    # the status value to return
+        int $0x80       # execute a system call
+
