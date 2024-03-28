3d1667be27682f4eb604d77fbf58e63e285a41c2
Kenan Kartal <contact@kenankartal.name>
2024-03-28 15:45:26 +0300

    Add 02_04-numbers

---
+++ 02_04-numbers/numbers.s
@@ -0,0 +1,18 @@
+.section .data
+# Floats
+.float 0.0
+.float 1.0
+.float -1.0
+.float 1.75
+.float 122.5
+.float 1.1
+.float 10000000000.0
+# Ints
+.int 0
+.int 1
+.int -1
+.int 175
+.int 4097
+.int 65536
+.int 100000000
+
